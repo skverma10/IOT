0067a1aa54411e8449a2f8535a7ae60659abdae6
SACHIN KUMAR VERMA <30444743+sachinkumarverma@users.noreply.github.com>
2018-02-23 01:14:01 +0530

    To config all connection

---
+++ config.lua
@@ -0,0 +1,14 @@
+keys = {
+		mqtt = {
+							server = "m11.cloudmqtt.com",
+							port = 11552,
+							user = "suntitlo",
+							password = "tJeugrFrkc0s"
+			  },
+		wifi = {
+
+							ssid = "sachin",
+							password = "aaaaaaaa"
+		      },
+		clientid = "node-1234"
+	}

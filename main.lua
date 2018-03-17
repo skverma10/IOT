2d5ea1ee98876a7af7c7c726f6e12af6fe8534db
SACHIN KUMAR VERMA <30444743+sachinkumarverma@users.noreply.github.com>
2018-03-17 22:06:59 +0530

    smac f, b, s, l, r

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 main = function (mqttbroker, client)
+			gpio.mode(1, gpio.OUTPUT)
 			gpio.mode(2, gpio.OUTPUT)
+			gpio.mode(3,gpio.OUTPUT)
 	if mqttbroker~= nil then
 		client:subscribe("/"..keys.clientid.."/led",0,function(client)
 			print("/"..keys.clientid.."/led")
@@ -8,13 +10,26 @@ main = function (mqttbroker, client)
 			function(client, topic, message)
 
 					if topic == "/"..keys.clientid.."/led" then
-						--gpio.write(2,gpio.LOW)
-						if message == 'on' then
-							gpio.write(2, gpio.	HIGH)
-							print("led is on")
+						if message == 'f' then
+							gpio.write(1,gpio.HIGH)
+							gpio.write(2,gpio.HIGH)
+							print("FORWORD ")
+						elseif message == 'l' then
+							gpio.write(1,gpio.HIGH)
+							gpio.write(2,gpio.LOW)
+							print("LEFT TURN ")
+						elseif message == 'r' then
+							gpio.write(1,gpio.LOW)
+							gpio.write(2,gpio.HIGH)
+							print("RIGHT TURN ")
+
+						elseif message =='b'	then
+							gpio.write(1,gpio.LOW)
+							gpio.write(2,gpio.LOW)
+							print("BACKWORD ")
 						else
-							gpio.write(2, gpio.LOW)
-							print("led is off")
+							gpio.write(3,gpio.HIGH)
+							print("STOP ")
 					  	end
 					end
 		    end)
